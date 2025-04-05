42ed3227d9dfe0cb136327f56168aac2ea938e43
Trinketer22 <trinketer22@localhost>
2025-04-05 21:36:17 +0300

    Convert to TOLK

--- wrappers/HighloadWalletV3.compile.ts
+++ wrappers/HighloadWalletV3.compile.ts
@@ -1,6 +1,6 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['contracts/highload-wallet-v3.func'],
+    lang: 'tolk',
+    entrypoint: 'contracts/highload-wallet-v3.tolk',
 };
